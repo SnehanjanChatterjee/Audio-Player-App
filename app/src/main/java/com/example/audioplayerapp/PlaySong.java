d89bfa5404e95f7a8dec3c7fcf26a76a33d73d68
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-12-14 03:26:39 +0530

    Selected song playing

--- app/src/main/java/com/example/audioplayerapp/PlaySong.java
+++ app/src/main/java/com/example/audioplayerapp/PlaySong.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,13 +18,18 @@ public class PlaySong extends AppCompatActivity {
     ArrayList<File> songsList;
     private MediaPlayer mediaPlayer;
     private String currentSongName;
-    private String position;
+    private Integer position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_song);
 
+        initialize();
+        playCurrentSong();
+    }
+
+    private void initialize() {
         this.txtView = findViewById(R.id.textView);
         this.previous = findViewById(R.id.previous);
         this.next = findViewById(R.id.next);
@@ -32,8 +38,14 @@ public class PlaySong extends AppCompatActivity {
         Bundle bundle = intent.getExtras();
         this.songsList = (ArrayList) bundle.getParcelableArrayList(MainActivity.EXTRA_SONGS_LIST);
         this.currentSongName = intent.getStringExtra(MainActivity.EXTRA_CURRENT_SONG_NAME);
-        this.position = intent.getStringExtra(MainActivity.EXTRA_POSITION);
+        this.position = intent.getIntExtra(MainActivity.EXTRA_POSITION, 0);
 
         this.txtView.setText(this.currentSongName);
+    }
+
+    private void playCurrentSong() {
+        Uri uri = Uri.parse(songsList.get(position).toString());
+        this.mediaPlayer = MediaPlayer.create(this, uri);
+        this.mediaPlayer.start();
     }
 }
